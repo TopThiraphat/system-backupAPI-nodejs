32cc407038510391ddac54b2100b203f6d88cdea
TopThiraphat <surapa.201041@gmail.com>
2023-11-21 14:11:46 +0700

    v1.0.0

---
+++ README.md
@@ -0,0 +1,52 @@
+# backupfileAPI-nodejs
+- เป็นระบบ backup file API จาก Link url โดยแปลงไฟล์ให้เป็น XML
+- ระบบจะ backup file API ทุกๆ 2 นาที 
+- เมื่อครบวันเวลาที่กำหนด ระบบจะ compress เป็น .zip ให้อัตโนมัติ 
+- แสดงผล output ทาง Terminal
+
+## Tech Stack in Project 
+
+- **Language**
+    - NodeJS
+- **libraries** 
+    - compressing 
+    - fs 
+    - node-fetch
+
+
+## Get Started
+
+Install project with npm
+
+```bash
+  cd backupfileAPI
+  npm install 
+```
+## Screenshots
+![App Screenshot](./Screenshots/2023-11-21_13-44.png)
+![App Screenshot](./Screenshots/2023-11-21_13-45.png)
+![App Screenshot](./Screenshots/2023-11-21_13-48.png)
+
+
+## Demo
+![Blog](https://img.shields.io/website?color=B71D25&style=flat-square&up_message=Demo&url=https%3A%2F%2Fmikkipastel.com)
+
+## Used By
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://github.com/TopThiraphat)
+
+## Support Me
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/R5R0RDJVK)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,101 @@
+const fs = require("fs");
+const path = require("path");
+const findRemoveSync = require("find-remove");
+const fetch = require("node-fetch");
+const compressing = require("compressing");
+
+// Link
+const baseURL =
+  "https://random-data-api.com/api/cannabis/random_cannabis?size=1&is_json=true";
+
+// Name Folder
+const directoryLinkForBackup = "link";
+const folderDirectory = "./file";
+const removeFile = 28;
+const timeBackup = 60000;
+
+// Check for directory
+console.log(
+  "--- Checking for directory " +
+    path.join(folderDirectory, directoryLinkForBackup)
+);
+
+// Fetch
+async function fetchLinkForBackup() {
+  /// start backup file
+  console.log("Backup Link running ...");
+  fs.exists(path.join(folderDirectory, directoryLinkForBackup), (exists) => {
+    if (exists) {
+      setInterval(async function () {
+        await fetch(baseURL)
+          .then((response) => response.text())
+          .then(async (data) => {
+            // custom name file
+            let place = "link";
+            let dateTime = new Date();
+            let dd = String(dateTime.getDate()).padStart(2, "0");
+            let mm = String(dateTime.getMonth() + 1).padStart(2, "0"); //January is 0!
+            let yyyy = dateTime.getFullYear();
+            let h = String(dateTime.getHours()).padStart(2, "0");
+            let mins = String(dateTime.getMinutes()).padStart(2, "0");
+            let sec = String(dateTime.getSeconds()).padStart(2, "0");
+            let removeDate_dd = dd;
+            let filename =
+              "/" + "DMY" + dd + mm + yyyy + "-" + "HMS" + h + mins + sec;
+
+            // Create File
+            try {
+              fs.writeFileSync(
+                path.join("./file", directoryLinkForBackup) +
+                  filename +
+                  "-" +
+                  place +
+                  ".xml",
+                data
+              );
+              let success = filename + "-" + place + ".xml";
+              console.log("Success : " + success);
+            } catch {
+              console.log(`Error writing file ${place}`);
+            }
+
+            //remove and zip
+            if (removeDate_dd == removeFile) {
+              await compressing.zip.compressDir(
+                `${folderDirectory}/${place}`,
+                `${folderDirectory}/compress/${place}/${
+                  "DMY" +
+                  dd +
+                  mm +
+                  yyyy +
+                  "-" +
+                  "HMS" +
+                  h +
+                  mins +
+                  sec +
+                  "-" +
+                  place
+                }.zip`
+              );
+              //
+              try {
+                findRemoveSync(path.join("./file", directoryLinkForBackup), {
+                  extensions: ".xml",
+                });
+                console.log(`Remove ${removeFile} days success !!!`);
+              } catch {
+                console.log(`Error remove ${removeFile} days`);
+              }
+            }
+          });
+      }, timeBackup);
+    } else {
+      console.log(`Backup ${place} Error folder not created !!!"`);
+    }
+  });
+}
+
+async function fetch_all() {
+  await fetchLinkForBackup();
+}
+fetch_all();
